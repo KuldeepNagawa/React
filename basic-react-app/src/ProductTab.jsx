2181fc4d4a3bb5ede122277c713d60de51cd3821
KuldeepNagawa <kuldeepnagawa5@gmail.com>
2025-05-26 17:33:22 +0530

    initial commit

---
+++ basic-react-app/src/ProductTab.jsx
@@ -0,0 +1,13 @@
+import Product from "./Product.jsx";
+
+function ProductTab() {
+    return (
+     <>
+      <Product title="phone" price="20,000"/>
+      <Product title="laptop" price="50,000"/>
+      <Product title="pen" price="10 "/>
+     </>
+    );
+}
+
+export default ProductTab;
---
+++ basic-react-app/src/Title.jsx
@@ -0,0 +1,8 @@
+function Title(){
+  return (<div>
+    <h1>I am the Title</h1>
+    <p>I am the description!</p>
+  </div>);
+}
+
+export default Title;
