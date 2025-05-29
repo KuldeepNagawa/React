0645789b704d5fa3e9b6f286045b0493d5104373
KuldeepNagawa <kuldeepnagawa5@gmail.com>
2025-05-29 16:44:14 +0530

    Lottery game

--- react-state-class2/src/App.jsx
+++ react-state-class2/src/App.jsx
@@ -1,13 +1,19 @@
 import './App.css';
 import LudoBoard from "./LudoBoard.jsx";
 import TodoList from "./TodoList.jsx";
+import Lottery from "./Lottery.jsx";
+import Ticket from "./Ticket.jsx";
+import TicketNum from './TicketNum.jsx';
+import { sum } from './helper.js';
+
 
 function App() {
- 
-
+  let winCondition = (ticket) =>{
+    return ticket[0] === 0;
+  }
   return (
     <>
-      <TodoList/>
+      <Lottery n={3} winCondition={winCondition}/>
     </>
   )
 }
